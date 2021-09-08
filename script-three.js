f1bb810a66f931417ea967230eafe28d7e5c3c6c
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-07 22:27:23 -0400

    add getAttribute

---
+++ script-three.js
@@ -0,0 +1,8 @@
+const body = document.body 
+const div = document.querySelector("div")
+const spanKion = document.querySelector("#kion")
+const spanKing = document.querySelector("#king")
+
+console.log(spanKing.getAttribute("id"))
+// getAttribute can be omitted 
+console.log(spanKing.title)
