37a208e68d7824107c5e60ccc605f92b8c0545d2
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-10 22:17:34 -0400

    add queryselector

---
+++ script-two.js
@@ -0,0 +1,7 @@
+const body = document.body 
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#kion")
+const spanBye = document.querySelector("#king")
+
+spanBye.remove()
+div.append(spanBye)
