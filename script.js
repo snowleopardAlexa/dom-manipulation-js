422f08f7da015f210177156ac7b37cdcfc1f8057
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-10 21:47:42 -0400

    append and appendChild

---
+++ script.js
@@ -0,0 +1,5 @@
+// add/append elements to the page
+const body = document.body 
+// appendChild and append are similar but with appendChild you can only append(add): div, spans, encor tags, NOT STRINGS
+// append will let you to append everything and STRINGS
+body.append("Hello Alexa", "Bye")
