8b864f7abb236f53b54d2e5729f47f79ef7e7f4e
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-10 21:49:47 -0400

    how to append element and add it to the page

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const body = document.body
 // appendChild and append are similar but with appendChild you can only append(add): div, spans, encor tags, NOT STRINGS
 // append will let you to append everything and STRINGS
 body.append("Hello Alexa", "Bye")
+
+// how to append element
+// we only created a reference to an element here 
+const div = document.createElement('div')
+// now we add div to the page
+body.append(div)
